8bf5363500d2c313e2208c91538aa9513444f012
Ricardo Antonio Duarte <ricardo.duarte@triersistemas.com.br>
2018-04-06 17:16:09 -0300

    Validação dos campos de Conta.

--- lib/openfriday/api/conta.ex
+++ lib/openfriday/api/conta.ex
@@ -15,5 +15,10 @@ defmodule Openfriday.Api.Conta do
     conta
     |> cast(attrs, [:dominio, :cliente_id, :email])
     |> validate_required([:dominio, :cliente_id, :email])
+    |> validate_number(:cliente_id, greater_than_or_equal_to: 1, less_than_or_equal_to: 999999)
+    |> validate_format(:email, ~r/@/)
+    |> unique_constraint(:dominio)
+    |> unique_constraint(:cliente_id)
+    |> unique_constraint(:email)
   end
 end
